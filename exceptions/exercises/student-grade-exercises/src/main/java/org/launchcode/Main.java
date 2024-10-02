674ec4f451a81643f8907307ca5f55064411ab65
adamstanislawsky <adamstanislawsky@gmail.com>
2024-10-01 21:33:35 -0500

    exceptions exercise pt1

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -1,10 +1,14 @@
 package org.launchcode;
 
+import java.sql.SQLOutput;
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
         // Test out your Divide() function!
+    Divide(10, 3);
+    Divide(2,0);
+
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -15,13 +19,20 @@ public class Main {
         // Test out your CheckFileExtension() function!
     }
 
-    public static void Divide(int x, int y)
-    {
+    public static void Divide(int x, int y) {
         // Write code here!
+        try {
+            int result = x/y;
+            System.out.println("Result:" + result);
+
+        } catch (ArithmeticException e) {
+            System.out.println("ERROR!!!! Cannot divide by zero");
+        }
+
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
-    }
+//    public static int CheckFileExtension(String fileName)
+//    {
+//        // Write code here!
+//    }
 }
