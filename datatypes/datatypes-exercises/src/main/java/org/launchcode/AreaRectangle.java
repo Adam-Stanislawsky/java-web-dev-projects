217f0fb93bc13f7dce3493848e2fd07afc734743
adamstanislawsky <adamstanislawsky@gmail.com>
2024-10-04 12:26:24 -0500

    working through old exercises

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/AreaRectangle.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/AreaRectangle.java
@@ -1,4 +1,16 @@
 package org.launchcode;
 
+import java.util.Scanner;
+
 public class AreaRectangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter height: ");
+        int height = input.nextInt();
+        System.out.println("please enter width: ");
+        int width = input.nextInt();
+        input.close();
+        int areaOfRectangle = width*height;
+        System.out.println("The area of your rectangle is " + areaOfRectangle);
+    }
 }
--- datatypes/datatypes-exercises/src/main/java/org/launchcode/MilesPerGallon.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/MilesPerGallon.java
@@ -1,4 +1,18 @@
 package org.launchcode;
 
+import java.util.Scanner;
+
 public class MilesPerGallon {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the miles you've driven: ");
+        double milesDriven = input.nextDouble();
+        System.out.println("now enter the gallons of gas you've used: ");
+        double gallonsGas = input.nextDouble();
+        input.close();
+
+        double milesPerGallon = milesDriven/gallonsGas;
+        System.out.println("Current MPG = "+ milesPerGallon);
+
+    }
 }
